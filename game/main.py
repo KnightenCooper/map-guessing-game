efd725d6532d0a61129b3e8c81102c226c126195
KnightenCooper <coo18028@byui.edu>
2021-10-25 20:01:36 -0500

    update the new start menu

--- game/main.py
+++ game/main.py
@@ -19,9 +19,9 @@ from start_menu_view_UPDATED import MyGame
 #     window.show_view(start_view)
 #     arcade.run()
 
-#     window = MyGame()
-#     window.setup()
-#     arcade.run()
+#     # window = MyGame()
+#     # window.setup()
+#     # arcade.run()
 # main()
 
 def main():
